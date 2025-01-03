29604377d52c2e8817ed4d798c574b7351827ed0
ʀᴀᴊᴀ ʙᴀʙᴜ <anilkumarmishraji80@gmail.com>
2025-01-04 02:23:26 +0530

    Update autoleave.py

--- PROMUSIC/plugins/misc/autoleave.py
+++ PROMUSIC/plugins/misc/autoleave.py
@@ -8,31 +8,37 @@ from PROMUSIC import app
 from PROMUSIC.core.call import PRO, autoend
 from PROMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
+
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(900):
+        while not await asyncio.sleep(900
+            
+        ):
             from PROMUSIC.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            "supergroup",
+                            "group",
+                            "channel",
                         ]:
+                            chat_id = i.chat.id
                             if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002342994330
-                                and i.chat.id != -1002296968230
+                                chat_id != config.LOGGER_ID
+                                and i.chat.id != -1002166858034
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(i.chat.id):
+                                if not await is_active_chat(chat_id):
                                     try:
-                                        await client.leave_chat(i.chat.id)
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
                                         left += 1
                                     except:
                                         continue
@@ -45,8 +51,7 @@ asyncio.create_task(auto_leave())
 
 async def auto_end():
     while not await asyncio.sleep(5):
-        ender = await is_autoend()
-        if not ender:
+        if not await is_autoend():
             continue
         for chat_id in autoend:
             timer = autoend.get(chat_id)
@@ -58,13 +63,13 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Tanu.stop_stream(chat_id)
+                    await PRO.stop_stream(chat_id)
                 except:
                     continue
                 try:
                     await app.send_message(
                         chat_id,
-                        "❖ ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
                     )
                 except:
                     continue
